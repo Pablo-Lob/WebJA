bcd67bb254777afec070ad035b75f0dfc30261bd
Pablo <jlobagon@myuax.com>
2026-01-17 10:35:05 +0100

    Add .env file for database configuration and enhance About section: update styles and include team member details

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './About.css';
 import aboutImageDefault from '../../assets/aboutUs.webp';
 import { useConfig } from '../../context/ConfigContext.jsx';
+import personAbout from '../../assets/hombreCitacion.webp';
 
 const About = () => {
     const { config } = useConfig();
@@ -18,15 +19,30 @@ const About = () => {
                     {/* Título dinámico */}
                     <h2 className="section-title">{getValue('about_title', 'About Us')}</h2>
 
-                    {/* Subtítulo dinámico (opcional, si no quieres editarlo déjalo fijo) */}
+                    {/* Subtítulo dinámico */}
                     <h3 className="section-subtitle">Forging Trust and Excellence</h3>
 
                     <div className="gold-divider-left"></div>
 
                     {/* Texto principal dinámico */}
                     <div className="about-text-content">
+                        <div className="person">
+                            <img
+                                src={getValue('about_person', personAbout)}
+                                alt={`Portrait of ${getValue('about_person_name', 'Jeyakumar J.')}`}
+                                className="about-person-image"
+                            />
+                            <div className="person-text">
+                                <p className="name-person-about">
+                                    {getValue('about_person_name', 'Jeyakumar J.')}
+                                </p>
+                                <p className="job-tittle-person-about">
+                                    {getValue('about_person_job', 'Head of Sales Department')}
+                                </p>
+                            </div>
+                        </div>
                         <p className="highlight-text">
-                            {getValue('about_text', "Headquartered in the strategic hub of Dubai, ITS Stones is your exclusive gateway to the world's most exquisite precious metals and luxury gemstones.")}
+                            {getValue('about_text', '"ITS-Stones is a premier global trading house specializing in the sourcing and importation of precious metals and high-grade gemstones. With decades of combined experience, we bridge the gap between ethically managed mines in Africa and South America and the world’s most demanding markets."')}
                         </p>
                     </div>
                 </div>
